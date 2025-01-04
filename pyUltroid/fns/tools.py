d3f47215ab3ca999fb8c7a4e329848856b4ca442
noctabit <maikelrss@proton.me>
2025-01-04 16:35:54 +0100

    Restore translation code

--- pyUltroid/fns/tools.py
+++ pyUltroid/fns/tools.py
@@ -661,6 +661,29 @@ def _package_rpc(text, lang_src="auto", lang_tgt="auto"):
     return freq
 
 
+def translate(*args, **kwargs):
+    headers = {
+        "Referer": "https://translate.google.co.in",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/47.0.2526.106 Safari/537.36",
+        "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+    }
+    x = requests.post(
+        "https://translate.google.co.in/_/TranslateWebserverUi/data/batchexecute",
+        headers=headers,
+        data=_package_rpc(*args, **kwargs),
+    ).text
+    response = ""
+    data = json.loads(json.loads(x[4:])[0][2])[1][0][0]
+    subind = data[-2]
+    if not subind:
+        subind = data[-1]
+    for i in subind:
+        response += i[0]
+    return response
+
+
 def cmd_regex_replace(cmd):
     return (
         cmd.replace("$", "")
